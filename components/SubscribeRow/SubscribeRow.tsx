9a9803b9a2d2e729cec11f9f8b126dc9474451ae
Andrew Reifman <andrewr23@gmail.com>
2024-01-09 14:00:00 -0500

    feat: style subscribe form

--- components/SubscribeRow/SubscribeRow.tsx
+++ components/SubscribeRow/SubscribeRow.tsx
@@ -17,34 +17,34 @@ export const SubscribeRow = forwardRef(function SubscribeRow(
   return (
     <div ref={ref} className={clsx(className, '@container')}>
       <form
-        className="@lg:flex-row flex w-full flex-col rounded-sm border"
+        className="@lg:flex-row @lg:border flex w-full flex-col gap-y-2 rounded-sm border-0"
         style={{ borderColor: color }}
       >
-        <div className="relative flex-1">
+        <div
+          className="@lg:border-0 relative flex-1 rounded-sm border"
+          style={{ borderColor: color, color }}
+        >
           <input
             id="email"
-            className="has-label w-full bg-transparent px-6 py-4"
+            className="peer h-12 w-full bg-transparent px-5 pt-4 placeholder:text-transparent focus:outline-none focus:ring-0"
             type="email"
             name="email"
             required
+            placeholder="Email address"
             autoComplete="email"
             autoCapitalize="none"
             aria-label="email"
-            value=""
           />
           <label
             htmlFor="email"
-            className="absolute left-4 top-1/2 -translate-y-1/2 text-sm font-light"
-            style={{
-              color,
-            }}
+            className="absolute left-5 top-1/2 -mt-3 -translate-y-1/2 text-xs font-light duration-100 ease-linear peer-placeholder-shown:mt-0 peer-placeholder-shown:text-sm peer-focus:-mt-3 peer-focus:text-xs"
           >
             {label}
           </label>
         </div>
 
         <button
-          className="block px-16 py-4 text-sm font-normal uppercase tracking-wider sm:inline-block"
+          className="@lg:rounded-none block rounded-sm px-16 py-4 text-sm font-normal uppercase tracking-wider focus:outline-gray-300 sm:inline-block"
           type="submit"
           style={{
             backgroundColor: color,
